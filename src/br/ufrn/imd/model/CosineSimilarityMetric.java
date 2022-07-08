fd3de89ebd40de96e754e77bf9722e7bab0c8d93
fernandals <lustosafk@gmail.com>
2022-07-08 15:00:09 -0300

    STAGE 02.2 - Finishing CosineSimilarity

--- src/br/ufrn/imd/model/CosineSimilarityMetric.java
+++ src/br/ufrn/imd/model/CosineSimilarityMetric.java
@@ -1,19 +1,44 @@
 package br.ufrn.imd.model;
 
 import org.apache.commons.text.similarity.CosineSimilarity;
-
 import java.util.Map;
+import java.util.HashMap;
 
 public class CosineSimilarityMetric extends SimilarityScore{
 
-	private Map<String, Integer> frequencyMap1;
-	private Map<String, Integer> frequencyMap2;
+	private Map<CharSequence, Integer> frequencyMap1;
+	private Map<CharSequence, Integer> frequencyMap2;
 	
+	public CosineSimilarityMetric(String s1, String s2) {
+		super();
+		super.setStr1(s1);
+		super.setStr2(s2);
+		this.buildFrequencyMaps();
+	}
+	
+	private void buildFrequencyMaps() {
+		this.frequencyMap1 = this.buildFrequencyMap(this.str1);
+		this.frequencyMap2 = this.buildFrequencyMap(this.str2);
+	}
+	
+	private Map<CharSequence, Integer> buildFrequencyMap(String s){
+		String[] splitted = s.split(" ");
+		Map<CharSequence, Integer> frequencyMap = new HashMap<CharSequence, Integer>();
+		
+		for (String split : splitted) {
+			frequencyMap.put(split, 1);
+		}
+		
+		return frequencyMap;
+	}
+
 	@Override
 	public double similarityScore() {
-		// TO DO
+		CosineSimilarity cos = new CosineSimilarity();
 		
-		return 1.0;
+		double score = cos.cosineSimilarity(this.frequencyMap1, this.frequencyMap2);
+		
+		return score;
 	}
 
 }
